0d8cc1dd286fe623dd9da1966be6145714f6dffc
Arun kumar <79135781+arunravi03@users.noreply.github.com>
2022-02-10 07:53:46 +0530

    Update convertToDec.js

--- convertToDec.js
+++ convertToDec.js
@@ -1,12 +1,3 @@
-// 2. Create a function to convert decimal string to decimal value
-
-/**
- * Step1 - Create a function with an input
- * Step2 - Then convert to a float value which should take upto 2 decimals
- * Step3 - Check whether the input is falsy value
- * Step 3.1 - If the input is falsy  and NaN retrun 0
- */
-
 function convertToDecimal(input) {
   const result = parseFloat(input).toFixed(2);
   if (isNaN(result)) {
